1365b57564238bb222ec62fa5d0a3d8febec1597
Minhyeok Park <opensource@pmh.codes>
2022-11-27 12:38:08 +0900

    chore: empty build branch

--- src/components/Footer/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react'
-import * as style from './style.module.scss'
-
-export const Footer: FC = () =>
-  <footer className={style.footer}>
-    <p>Minhyeok Park &copy; 2022.</p>
-    <a href="mailto:webmaster@pmh.codes">webmaster(at)pmh(dot)codes</a>
-  </footer>
--- src/components/Links/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react'
-import * as style from './style.module.scss'
-
-export const Links: FC = () =>
-  <ul className={style.links}>
-    <li><a href="https://portfolio.pmh.codes">My Portfolio</a></li>
-    <li><a href="https://remi.pmh.codes">RemiDrive</a></li>
-  </ul>
